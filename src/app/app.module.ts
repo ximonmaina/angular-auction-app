9834499ad8548f1ab8e6eedaa7a42b902b672f04
ximonmaina <simonmaina721@gmail.com>
2019-05-28 04:11:35 +0300

    initial application setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
+import {ProductService} from './shared/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
